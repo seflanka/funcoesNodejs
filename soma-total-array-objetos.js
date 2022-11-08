fe18ef9dae46c4785a4ea6ae1c4cd0e3b757a837
Guilherme de  Souza <91706877+seflanka@users.noreply.github.com>
2022-11-08 10:10:09 -0300

    att commit

--- soma-total-array-objetos.js
+++ soma-total-array-objetos.js
@@ -1,17 +1,18 @@
 
 //Aqui é onde damos o .length no array para saber o total
-const quantidadeDoLength = 10
+const quantidadeDoLength = 10;
 
 //Setamos um array vasio para poer adicionar o valor de cada item do array
-var arrayLength = []
+var arrayLength = [];
 
 //Usamos um for para interar cada valor no array vazio
-for ( var i = 0; i <= quantidadeDoLength; i++) {
-    var criandoArray = arrayLength.unshift(i);
+for ( var x = 0; x <= quantidadeDoLength; x++) {
+    var criandoArray = arrayLength.unshift(x);
     //Dentro da função do unshifth devemos resgatar os valores passando o camido dos objetos
-}
+};
 
 //Usa o metodo reduce((a,b) => a + b), para somar os valores dentro do array.
 const somaTotal = arrayLength.reduce((a, b) => a+ b ).toFixed([2]);
 
-console.log(somaTotal)
+console.log(somaTotal);
+
